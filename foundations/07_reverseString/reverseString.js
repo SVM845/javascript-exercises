55b33c3a6def95561a08839d1c4dda6ce841f012
Hesam Sormeyli <hesam1385hs@gmail.com>
2026-08-03 19:14:32 +0330

    07
    Done

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    string = string.split("");
+    let result = '';
+    for (let i = string.length - 1; i >= 0; i-- ){
+        result += string[i];
+    }
+    return result;
+    
+};  
 
-};
+reverseString("hello there")
 
 // Do not edit below this line
 module.exports = reverseString;
