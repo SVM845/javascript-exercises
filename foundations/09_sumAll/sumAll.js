6217d0e0194264a3379c46968021f1b83a713cd2
Hesam Sormeyli <hesam1385hs@gmail.com>
2026-08-04 16:08:08 +0330

    09
    Done

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
 
+    if (typeof num1 !== "number" || typeof num2 !== "number" || !Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0) {
+        return 'ERROR'
+    }
+
+    let resultArray = [];
+    if (num2 > num1) {
+        for (let i = num1; i <= num2; i++) {
+            resultArray.push(i);
+        }
+        return resultArray.reduce((sum, current) => sum + current);
+
+
+    } else if (num1 > num2) {
+        for (let i = num2; i <= num1; i++) {
+            resultArray.push(i);
+        }
+        return resultArray.reduce((sum, current) => sum + current);
+
+
+    }
 };
+
+sumAll(5, 10);
 
 // Do not edit below this line
 module.exports = sumAll;
