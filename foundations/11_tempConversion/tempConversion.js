b69886eead4c8322b22e951a9566d39a9242d0e5
Hesam Sormeyli <hesam1385hs@gmail.com>
2026-08-04 17:28:36 +0330

    11
    Done

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celsius = (temp - 32) * (5/9);
+  let roundNum = Math.round(celsius * 10) / 10;
+  return roundNum;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  fahrenheit = (temp * (9/5)) + 32;
+  let roundNum = Math.round(fahrenheit * 10) / 10;
+  return roundNum;
 };
 
 // Do not edit below this line
