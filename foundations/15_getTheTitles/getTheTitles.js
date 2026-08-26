d63caea4ba98992f6209328d46bdb54404701878
Hesam Sormeyli <hesam1385hs@gmail.com>
2026-08-26 22:58:24 +0330

    15
    Done

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,16 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    return books.map(item => item.title);
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+// function getTitles(books){
+//     console.log(books.map(item => item.title));
+// };
+
+// getTitles([
+//     {title: "book1"},
+//     {title: "book2"},
+//     {title: "book3"},
+// ]);
