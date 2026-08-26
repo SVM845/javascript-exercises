aa975ac1bc9bcf4d38189630c9ebfead1f3e6a5f
Hesam Sormeyli <hesam1385hs@gmail.com>
2026-08-26 23:44:00 +0330

    16
    Done +_+

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    const year = new Date().getFullYear();
+    let oldest = people.sort((a, b) => {
+        if (a.yearOfBirth && a.yearOfDeath && b.yearOfBirth && b.yearOfDeath){
+        return (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth)}
+        else if (!a.yearOfDeath || !b.yearOfDeath){return (year - a.yearOfBirth) - (year - b.yearOfBirth)}
+    });
+    let i = oldest.length - 1;
+    return oldest[i];
 
 };
 
