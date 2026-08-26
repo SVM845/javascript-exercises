4d1c1695b249d3e69cc595d8c59f102c531d7283
Hesam Sormeyli <hesam1385hs@gmail.com>
2026-08-26 22:23:33 +0330

    13 Done

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let cleanString = string.replace(/[.,!? ]/g, "").toLowerCase();
+    let result = cleanString
+        .split("")
+        .reverse()
+        .join("");
 
+    // console.log(result);
+    if (cleanString == result) { return true }
+    else { return false };
 };
+
+// console.log(palindromes("123321"));
 
 // Do not edit below this line
 module.exports = palindromes;
