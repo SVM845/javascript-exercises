b88d191f4bca14fd1e97fab8cc1368ecbb2d5256
Hesam Sormeyli <hesam1385hs@gmail.com>
2026-08-26 20:48:27 +0330

    12
    Done

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  let result = num1 - num2;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((total, current) => {
+    return total + current;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let result = arr.reduce((total, current) => {
+    return total * current;
+  });
+  return result;
 };
 
-const power = function() {
-	
+const power = function (base, expo) {
+  let temp = 1;
+  for (let i = 1; i <= expo; i++) {
+    temp = temp * base;
+  }
+  return temp;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let temp = 1;
+  for (let i = 2; i <= num; i++) {
+    temp = temp * i;
+  }
+  return temp;
 };
 
 // Do not edit below this line
